c179c58b4d07212cffbb8d6719213a66766f000e
Aaditya Mishra <aadityamishra216@gmail.com>
2023-04-02 12:08:12 +0530

    data_service created

---
+++ lib/data_service.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+class DataService {
+
+  void getWeather(String city) {
+
+    final queryParameters = {
+      'q': city,
+      'appid': '29d6a6996e6ad2e669d2eeb133cedcd0'
+    };
+
+    final uri = Uri.https(
+        'api.openweathermap.org', '/data/2.5/weather', queryParameters);
+  }
+}
